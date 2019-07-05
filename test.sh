b915e5d24cd09773384bbf8f8ec349bd26d41090
Kai <wang319@usc.edu>
2019-07-04 23:29:34 -0700

    0704

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 SEED=3824
-FILENAME="0704-2200"
+FILENAME="0704-2300"
 METHOD=0
-BUDGET=1
+BUDGET=2
 NODES=15
 
 python3 pathProbabilities.py --epochs=25 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=1 --number-nodes=$NODES --number-graphs=100 --number-samples=1 --learning-rate=0.01 --prob=0.35 --feature-size=3 --number-sources=2 --number-targets=2
