e5daebaf1d6054d4de0980ea3a703b91532462d1
Kai <wang319@usc.edu>
2019-07-26 07:36:09 -0700

    07266

--- multitest.sh
+++ multitest.sh
@@ -1,15 +1,25 @@
-FILENAME="0724"
+FILENAME="0725"
 BUDGET=2
-NODES=20
-METHOD=1
+NODES=15
 
-for VAR in {1..10}
+for VAR in {1..5}
 	do
 		echo $VAR
 		SEED=$VAR
 		MINCUT=0
+		METHOD=0
 		python3 pathProbabilities.py --epochs=30 --fixed-graph=0 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=0 --number-nodes=$NODES --number-graphs=1 --number-samples=50 --learning-rate=0.01 --prob=0.3 --feature-size=5 --number-sources=2 --number-targets=2 --mincut=$MINCUT
+
+		MINCUT=0
+		METHOD=1
+		python3 pathProbabilities.py --epochs=30 --fixed-graph=0 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=0 --number-nodes=$NODES --number-graphs=1 --number-samples=50 --learning-rate=0.01 --prob=0.3 --feature-size=5 --number-sources=2 --number-targets=2 --mincut=$MINCUT
+
 		MINCUT=1
+		METHOD=0
+		python3 pathProbabilities.py --epochs=30 --fixed-graph=0 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=0 --number-nodes=$NODES --number-graphs=1 --number-samples=50 --learning-rate=0.01 --prob=0.3 --feature-size=5 --number-sources=2 --number-targets=2 --mincut=$MINCUT
+
+		MINCUT=1
+		METHOD=1
 		python3 pathProbabilities.py --epochs=30 --fixed-graph=0 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=0 --number-nodes=$NODES --number-graphs=1 --number-samples=50 --learning-rate=0.01 --prob=0.3 --feature-size=5 --number-sources=2 --number-targets=2 --mincut=$MINCUT
 	done
 
