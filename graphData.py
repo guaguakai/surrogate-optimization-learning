09048ab726706bd1a6c2d51f046093673f766477
AdityaMate <adityamate22@gmail.com>
2019-08-05 22:24:10 -0400

    phi varying with distance from target

--- graphData.py
+++ graphData.py
@@ -104,13 +104,16 @@ def generatePhi(G, possible_ranges=[(0,0.5), (0.5,5), (5,8)], fixed_phi=0):
 
     elif fixed_phi == 0 or fixed_phi == 1:
         for node in list(G.nodes()):
-            
+            # Compute distance from the target: 
+            dist_target=min([nx.shortest_path_length(G, source=node, target=target) for target in targets])
             if node in sources:
                 range_of_phi=possible_ranges[0]
             elif node in targets:
                 range_of_phi=possible_ranges[-1]
             else:
                 range_of_phi=possible_ranges[1]
+            
+            range_of_phi=(2**(5-dist_target),2**(6-dist_target))
             #node_features=np.random.randn(feature_length)
             # TODO: Use a better feature computation for a given node
             #r=np.random.choice(len(possible_ranges))
@@ -342,7 +345,7 @@ def attackerOracle(G, coverage_probs, phi, omega=4, num_paths=100):
     simulated_defender_utility_list = []
     for _ in range(num_paths):
         path = getMarkovianWalk(G, biased_probs)
-        path = getSimplePath(G, path) # TODO
+        #path = getSimplePath(G, path) # TODO
         path_list.append(path)
 
         defender_utility = -G.node[path[-1][1]]['utility']
