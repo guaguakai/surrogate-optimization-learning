d703c15505aaa789c2655d589f6239fbcdbb847a
Aditya Mate <amate@holylogin02.rc.fas.harvard.edu>
2019-08-07 09:36:09 -0400

    dummy

--- runExp.sh
+++ runExp.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1                # Ensure that all cores are on one machine
 #SBATCH -t 2-00:10          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p serial_requeue   # Partition to submit to
-#SBATCH --mem=40000           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=20000           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_variablephi_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 
--- runExp3.sh
+++ runExp3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -n 4                # Number of cores
 #SBATCH -N 1                # Ensure that all cores are on one machine
-#SBATCH -t 2-00:10          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 1-10:10          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p serial_requeue   # Partition to submit to
-#SBATCH --mem=40000           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=5000           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_variablephi_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 
