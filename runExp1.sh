f666078d3913a9fb2078c770078f648071d69f56
Aditya Mate <amate@holylogin04.rc.fas.harvard.edu>
2019-08-09 15:35:40 -0400

    collecting intermediate results

--- runExp1.sh
+++ runExp1.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1                # Ensure that all cores are on one machine
 #SBATCH -t 1-10:10          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p serial_requeue   # Partition to submit to
-#SBATCH --mem=5000           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=4000           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_variablephi_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 
