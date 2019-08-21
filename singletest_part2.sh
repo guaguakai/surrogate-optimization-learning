56a349cdc9f7fb610d31ec00f02d271b817cf13b
Kai <wang319@usc.edu>
2019-08-20 19:30:04 -0700

    update

--- singletest_part2.sh
+++ singletest_part2.sh
@@ -8,6 +8,6 @@ for VAR in {11..20}
 		SEED=$VAR
 		MINCUT=0
 		METHOD=1
-		python3 pathProbabilities.py --epochs=30 --fixed-graph=2 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=1 --number-nodes=$NODES --number-graphs=1 --number-samples=50 --learning-rate=0.002 --prob=0.3 --feature-size=5 --number-sources=2 --number-targets=2 --mincut=$MINCUT
+		python3 pathProbabilities.py --epochs=30 --fixed-graph=0 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=1 --number-nodes=$NODES --number-graphs=1 --number-samples=50 --learning-rate=0.002 --prob=0.3 --feature-size=5 --number-sources=2 --number-targets=2 --mincut=$MINCUT
 
 	done
