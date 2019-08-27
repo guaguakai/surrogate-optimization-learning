755e596381b244f3d900b0a8e7c7f2717c4a0adb
Kai <wang319@usc.edu>
2019-08-27 12:52:19 -0400

    0827 local changes

--- singletest_part2.sh
+++ singletest_part2.sh
@@ -1,9 +1,11 @@
-FILENAME="0825-server"
+FILENAME="0827-server"
 BUDGET=2
-NODES=30
+NODES=40
+EPOCHS=51
 
 echo $VAR
 SEED=$VAR
 MINCUT=0
-METHOD=0
-python3 pathProbabilities.py --epochs=31 --fixed-graph=0 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=1 --number-nodes=$NODES --number-graphs=10 --number-samples=10 --learning-rate=0.002 --prob=0.2 --feature-size=16 --number-sources=2 --number-targets=2 --mincut=$MINCUT
+METHOD=1
+python3 pathProbabilities.py --epochs=$EPOCHS --fixed-graph=0 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=1 --number-nodes=$NODES --number-graphs=10 --number-samples=10 --learning-rate=0.01 --prob=0.2 --feature-size=8 --number-sources=2 --number-targets=2 --mincut=$MINCUT
+
