c9a0de622e94efafb0a0266e400e1de7874921d3
b01901169 <b01901169@ntu.edu.tw>
2019-09-28 15:55:29 -0400

    misc

--- singletest_part1.sh
+++ singletest_part1.sh
@@ -3,10 +3,11 @@
 # NODES=30
 # EPOCHS=101
 # PROB=0.2
+LR=0.001
 
 echo $NODES
 echo $VAR
 SEED=$VAR
 METHOD=0
-python3 blockQP.py --epochs=$EPOCHS --fixed-graph=0 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=1 --number-nodes=$NODES --number-graphs=1 --number-samples=100 --learning-rate=0.0001 --prob=$PROB --feature-size=16 --number-sources=2 --number-targets=2 --noise=$NOISE
+python3 blockQP.py --epochs=$EPOCHS --fixed-graph=0 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=1 --number-nodes=$NODES --number-graphs=1 --number-samples=100 --learning-rate=$LR --prob=$PROB --feature-size=16 --number-sources=2 --number-targets=2 --noise=$NOISE
 
