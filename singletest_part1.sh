66745182e534b75d746cbe7d7c7af160cdb71e95
guaguakai <kaiwang@g.harvard.edu>
2019-10-09 20:23:39 -0400

    1009 2000 local

--- singletest_part1.sh
+++ singletest_part1.sh
@@ -3,11 +3,11 @@
 # NODES=30
 # EPOCHS=101
 # PROB=0.2
-LR=0.001
+LR=0.002
 
 echo $NODES
 echo $VAR
 SEED=$VAR
 METHOD=0
-python3 blockQP.py --epochs=$EPOCHS --fixed-graph=0 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=1 --number-nodes=$NODES --number-graphs=1 --number-samples=100 --learning-rate=$LR --prob=$PROB --feature-size=16 --number-sources=2 --number-targets=2 --noise=$NOISE
+python3 blockQP.py --epochs=$EPOCHS --fixed-graph=0 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=1 --number-nodes=$NODES --number-graphs=1 --number-samples=100 --learning-rate=$LR --prob=$PROB --feature-size=8 --number-sources=2 --number-targets=2 --noise=$NOISE
 
