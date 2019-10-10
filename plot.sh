fddece62eaf3c0d8218014f2db4e8857fd84b86f
guaguakai <kaiwang@g.harvard.edu>
2019-10-10 15:21:10 -0400

    1600 update

--- plot.sh
+++ plot.sh
@@ -1,8 +1,8 @@
 FILENAME='1010-1400-server'
 
-NODES=10
+NODES=20
 NOISE=0.0
-BUDGET=3
-PROB=0.3
+BUDGET=2
+PROB=0.2
 
 python3 generate_fig4.py --filename=$FILENAME --number-nodes=$NODES --noise=$NOISE --budget=$BUDGET --prob=$PROB
