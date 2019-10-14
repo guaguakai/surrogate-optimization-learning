fixing nan bug in derivative.py

blockQP.py
@@ -79,7 +79,7 @@ def learnEdgeProbs_simple(train_data, validate_data, test_data, f_save, f_time, 
                 Fv_torch   = torch.as_tensor(Fv, dtype=torch.float)
                 edge_index = torch.Tensor(list(nx.DiGraph(G).edges())).long().t()
                 phi_pred   = net2(Fv_torch, edge_index).view(-1) if epoch >= 0 else phi_true # when epoch < 0, testing the optimal loss and defender utility
-                phi_pred.require_grad = True
+                # phi_pred.require_grad = True
 
                 unbiased_probs_pred = phi2prob(G, phi_pred)
                 biased_probs_pred = generate_EdgeProbs_from_Attractiveness(G, coverage_prob,  phi_pred, omega=omega)
@@ -158,15 +158,15 @@ def learnEdgeProbs_simple(train_data, validate_data, test_data, f_save, f_time, 
                 # print(batch_loss)
                 if (iter_n%batch_size == (batch_size-1)) and (epoch > 0) and (mode == "training"):
                     optimizer.zero_grad()
-                    try:
-                        batch_loss.backward()
-                        torch.nn.utils.clip_grad_norm_(net2.parameters(), max_norm=max_norm) # gradient clipping
-                        # print(torch.norm(net2.gcn1.weight.grad))
-                        # print(torch.norm(net2.gcn2.weight.grad))
-                        # print(torch.norm(net2.fc1.weight.grad))
-                        optimizer.step()
-                    except:
-                        print("no grad is backpropagated...")
+                    # try:
+                    batch_loss.backward()
+                    torch.nn.utils.clip_grad_norm_(net2.parameters(), max_norm=max_norm) # gradient clipping
+                    # print(torch.norm(net2.gcn1.weight.grad))
+                    # print(torch.norm(net2.gcn2.weight.grad))
+                    # print(torch.norm(net2.fc1.weight.grad))
+                    optimizer.step()
+                    # except:
+                    #     print("no grad is backpropagated...")
                     batch_loss = 0
 
             if (epoch > 0) and (mode == "validating"):
@@ -265,6 +265,8 @@ def getDefUtility(single_data, unbiased_probs_pred, path_model, omega=4, verbose
         # But theoretically they should perform roughly the same...
 
         Q = obj_hessian_matrix_form(pred_optimal_coverage, G, unbiased_probs_pred, U, initial_distribution, edge_set, omega=omega)
+        jac = dobj_dx_matrix_form(pred_optimal_coverage, G, unbiased_probs_pred, U, initial_distribution, edge_set, omega=omega, lib=torch)
+
         Q_sym = (Q + Q.t()) / 2
     
         eigenvalues, eigenvectors = np.linalg.eig(Q_sym)
@@ -273,7 +275,6 @@ def getDefUtility(single_data, unbiased_probs_pred, path_model, omega=4, verbose
         Q_regularized = (Q_sym + torch.eye(len(edge_set)) * max(0, -min(eigenvalues) + reg_const))
         # new_eigenvalues, new_eigenvectors = np.linalg.eig(Q_regularized)
         
-        jac = dobj_dx_matrix_form(pred_optimal_coverage, G, unbiased_probs_pred, U, initial_distribution, edge_set, omega=omega, lib=torch)
         p = jac.view(1, -1) - pred_optimal_coverage[edge_set] @ Q_regularized
   
         try:

derivative.py
@@ -16,14 +16,14 @@ import autograd
 from gurobipy import *
 
 REG = 0.0
-MEAN_REG = 0.001
+MEAN_REG = 0.0
 
 def phi2prob(G, phi): # unbiased but no need to be normalized. It will be normalized later
     N=nx.number_of_nodes(G)
     adj = torch.Tensor(nx.adjacency_matrix(G).toarray())
     adj_phi = adj * phi - (1 - adj) * 100 # adding -100 to all the non-adjacent entries
     unbiased_probs = torch.nn.Softmax(dim=1)(adj_phi)
-    # unbiased_probs = unbiased_probs * adj
+    unbiased_probs = unbiased_probs * adj
 
     # unbiased_probs = adj * torch.exp(phi)
     # unbiased_probs = unbiased_probs / torch.sum(unbiased_probs, keepdim=True, dim=1)
@@ -58,10 +58,10 @@ def prob2unbiased(G, coverage_probs, biased_probs, omega): # no need to be norma
     unbiased_probs_sum = torch.sum(unbiased_probs, keepdim=True, dim=1) # + MEAN_REG
     # unbiased_probs[unbiased_probs_sum != 0] /= unbiased_probs_sum[unbiased_probs_sum != 0]
     unbiased_probs /= unbiased_probs_sum
-    unbiased_probs[row_sum == 0] = 0
+    # unbiased_probs[row_sum == 0] = 0
     # unbiased_probs = unbiased_probs / (torch.sum(unbiased_probs, keepdim=True, dim=1))
     # unbiased_probs = unbiased_prob * adj
-    # unbiased_probs[torch.isnan(unbiased_probs)] = 0
+    unbiased_probs[torch.isnan(unbiased_probs)] = 0
 
     return unbiased_probs
 
@@ -131,12 +131,13 @@ def objective_function_matrix_form(coverage_probs, G, unbiased_probs, U, initial
         coverage_prob_matrix[e[1]][e[0]]=coverage_probs[i] # for undirected graph only
 
     adj = torch.Tensor(nx.adjacency_matrix(G).toarray())
-    exponential_term = torch.exp(- omega * coverage_prob_matrix) * unbiased_probs + MEAN_REG
+    exponential_term = torch.exp(- omega * coverage_prob_matrix) * unbiased_probs # + MEAN_REG
     row_sum = torch.sum(exponential_term, dim=1)
-    marginal_prob = exponential_term / torch.sum(exponential_term, keepdim=True, dim=1)
+    marginal_prob = torch.zeros_like(exponential_term)
+    marginal_prob[row_sum != 0] = exponential_term[row_sum != 0] / torch.sum(exponential_term, keepdim=True, dim=1)[row_sum != 0]
     # marginal_prob = marginal_prob * adj
     # marginal_prob[torch.isnan(marginal_prob)] = 0
-    marginal_prob[row_sum == 0] = 0
+    # marginal_prob[row_sum == 0] = 0
 
     state_prob = marginal_prob * (1 - coverage_prob_matrix)
     caught_prob = 1 - torch.sum(state_prob, keepdim=True, dim=1)
@@ -172,10 +173,11 @@ def dobj_dx_matrix_form(coverage_probs, G, unbiased_probs, U, initial_distributi
     adj = torch.Tensor(nx.adjacency_matrix(G).toarray())
     exponential_term = torch.exp(-omega * coverage_prob_matrix) * unbiased_probs + MEAN_REG
     row_sum = torch.sum(exponential_term, dim=1)
-    marginal_prob = exponential_term / torch.sum(exponential_term, keepdim=True, dim=1)
+    marginal_prob = torch.zeros_like(exponential_term)
+    marginal_prob[row_sum != 0] = exponential_term[row_sum != 0] / torch.sum(exponential_term, keepdim=True, dim=1)[row_sum != 0]
     # marginal_prob = marginal_prob * adj
     # marginal_prob[torch.isnan(marginal_prob)] = 0
-    marginal_prob[row_sum == 0] = 0
+    # marginal_prob[row_sum == 0] = 0
 
     state_prob = marginal_prob * (1 - coverage_prob_matrix)
     caught_prob = torch.sum(marginal_prob * coverage_prob_matrix, keepdim=True, dim=1)

graphData.py
@@ -526,11 +526,11 @@ def generateSyntheticData(node_feature_size, omega=4,
                 for e in path:
                     empirical_transition_probs[e[0]][e[1]]+=1
 
-            row_sum = torch.sum(empirical_transition_probs, dim=1)
+            # row_sum = torch.sum(empirical_transition_probs, dim=1)
             adj = torch.Tensor(nx.adjacency_matrix(G).toarray())
             empirical_transition_probs = empirical_transition_probs / torch.sum(empirical_transition_probs, dim=1, keepdim=True)
-            empirical_transition_probs[row_sum == 0] = 0
-            # empirical_transition_probs[torch.isnan(empirical_transition_probs)] = 0
+            # empirical_transition_probs[row_sum == 0] = 0
+            empirical_transition_probs[torch.isnan(empirical_transition_probs)] = 0
             # empirical_transition_probs = empirical_transition_probs * adj
             # print('biased:', empirical_transition_probs)
             empirical_unbiased_probs = prob2unbiased(G, private_coverage_prob, empirical_transition_probs, omega)