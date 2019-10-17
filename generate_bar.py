963b690e3278c46b51ef052bd1178696ed718bfe
guaguakai <kaiwang@g.harvard.edu>
2019-10-17 17:04:53 -0400

    1017 1700

--- generate_bar.py
+++ generate_bar.py
@@ -71,23 +71,28 @@ def read_file(filename, method):
     print(method, len(data.keys()))
     print(sorted(data.keys()))
 
-    final_loss = np.mean(final_loss_list)
-    final_defu = np.mean(final_defu_list)
-    opt_loss = np.mean(opt_loss_list)
-    opt_defu = np.mean(opt_defu_list)
-    init_loss = np.mean([data[key]['te_loss'][0] for key in data])
-    init_defu = np.mean([data[key]['te_defu'][0] for key in data])
+    final_loss_list = np.array(final_loss_list)
+    final_defu_list = np.array(final_defu_list)
+    opt_loss_list = np.array(opt_loss_list)
+    opt_defu_list = np.array(opt_defu_list)
+
+    init_loss_list = np.array([data[key]['te_loss'][0] for key in data])
+    init_defu_list = np.array([data[key]['te_defu'][0] for key in data])
         
-    return final_loss, final_defu, opt_loss, opt_defu, init_loss, init_defu
+    return final_loss_list, final_defu_list, opt_loss_list, opt_defu_list, init_loss_list, init_defu_list
 
 def generatePlot(bar_list, labels, filename):
     
     fig, axs = plt.subplots(1, len(bar_list))
 
     for i in range(len(bar_list)):
-        data_opt, data_df, data_2s, data_init = bar_list[i]
-        axs[i].bar(labels, [data_opt, data_df, data_2s, data_init])
+        data_df, data_2s, data_init = bar_list[i]
+        axs[i].bar(labels, [data_df, data_2s, data_init])
         # axs[len(xy_list) + i].yticks(np.arange(2), (label_df, label_2d))
+
+    axs[0].title.set_text('Mean Regret')
+    axs[1].title.set_text('Median Regret')
+
 
     #epochs = len(train_loss) - 1
     #x=range(-1, epochs)
@@ -97,7 +102,7 @@ def generatePlot(bar_list, labels, filename):
 
 if __name__=='__main__':
     
-    labels = ['OPT', 'DF-block', '2S', 'Initial']
+    labels = ['DF-block', '2S', 'Initial']
     print(labels)
     # ==================== Parser setting ==========================
     parser = argparse.ArgumentParser(description='GCN Interdiction')
@@ -123,10 +128,25 @@ if __name__=='__main__':
     file2 = "results/random/{}_{}_n{}_p{}_b{}_noise{}.csv".format(filename, 'two-stage', GRAPH_N_LOW, GRAPH_E_PROB_LOW, DEFENDER_BUDGET, NOISE_LEVEL)
 
     #to_plot,x,epochs,=return_yaxis(f)
-    df_loss, df_defu, opt_loss, opt_defu, init_loss, init_defu = read_file(file1, 'decision-focused')
-    ts_loss, ts_defu, _, _, _, _ = read_file(file2, 'two-stage')
+    df_loss_list, df_defu_list, df_opt_loss_list, df_opt_defu_list, df_init_loss_list, df_init_defu_list = read_file(file1, 'decision-focused')
+    ts_loss_list, ts_defu_list, ts_opt_loss_list, ts_opt_defu_list, ts_init_loss_list, ts_init_defu_list = read_file(file2, 'two-stage')
+
+    df_loss_median = np.median(df_loss_list - df_opt_loss_list)
+    df_defu_median = np.median(df_defu_list - df_opt_defu_list)
+    init_loss_median = np.median(df_init_loss_list - df_opt_loss_list)
+    init_defu_median = np.median(df_init_defu_list - df_opt_defu_list)
+    ts_loss_median = np.median(ts_loss_list - ts_opt_loss_list)
+    ts_defu_median = np.median(ts_defu_list - ts_opt_defu_list)
+
+    df_loss_mean   = np.mean(df_loss_list - df_opt_loss_list)
+    df_defu_mean   = np.mean(df_defu_list - df_opt_defu_list)
+    init_loss_mean = np.mean(df_init_loss_list - df_opt_loss_list)
+    init_defu_mean = np.mean(df_init_defu_list - df_opt_defu_list)
+    ts_loss_mean   = np.mean(ts_loss_list - ts_opt_loss_list)
+    ts_defu_mean   = np.mean(ts_defu_list - ts_opt_defu_list)
     
-    bar_list = [(opt_loss, df_loss, ts_loss, init_loss), (opt_defu, df_defu, ts_defu, init_defu)]
+    bar_list = [(df_defu_mean,   ts_defu_mean,   init_defu_mean),
+                (df_defu_median, ts_defu_median, init_defu_median)]
     print('loss:', bar_list[0])
     print('defu:', bar_list[1])
     
