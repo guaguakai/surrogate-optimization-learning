a1e719bf2224c7794a9a268571573918a25ec304
guaguakai <kaiwang@g.harvard.edu>
2019-10-20 22:36:35 -0400

    adding hybrid analysis

--- generate_bar.py
+++ generate_bar.py
@@ -88,8 +88,8 @@ def generatePlot(bar_list, labels, filename):
     fig, axs = plt.subplots(1, len(bar_list))
 
     for i in range(len(bar_list)):
-        data_df, data_2s, data_init = bar_list[i]
-        axs[i].bar(labels, [data_df, data_2s, data_init])
+        data_df, data_2s, data_hb, data_init = bar_list[i]
+        axs[i].bar(labels, [data_df, data_2s, data_hb, data_init])
         # axs[len(xy_list) + i].yticks(np.arange(2), (label_df, label_2d))
 
     axs[0].title.set_text('Mean Regret')
@@ -104,7 +104,7 @@ def generatePlot(bar_list, labels, filename):
 
 if __name__=='__main__':
     
-    labels = ['DF-block', '2S', 'Initial']
+    labels = ['DF-block', '2S', 'Hybrid', 'Initial']
     print(labels)
     # ==================== Parser setting ==========================
     parser = argparse.ArgumentParser(description='GCN Interdiction')
@@ -128,12 +128,14 @@ if __name__=='__main__':
     #f="./results/0808normto10_linearphi_dist_tenruns_fullINIT_decision-focused_n20_p0.3_b2.0_global - Copy.csv"
     file1 = "results/random/{}_{}_n{}_p{}_b{}_noise{}.csv".format(filename, 'block-decision-focused', GRAPH_N_LOW, GRAPH_E_PROB_LOW, DEFENDER_BUDGET, NOISE_LEVEL)
     file2 = "results/random/{}_{}_n{}_p{}_b{}_noise{}.csv".format(filename, 'two-stage', GRAPH_N_LOW, GRAPH_E_PROB_LOW, DEFENDER_BUDGET, NOISE_LEVEL)
+    file3 = "results/random/{}_{}_n{}_p{}_b{}_noise{}.csv".format(filename, 'hybrid', GRAPH_N_LOW, GRAPH_E_PROB_LOW, DEFENDER_BUDGET, NOISE_LEVEL)
 
     #to_plot,x,epochs,=return_yaxis(f)
     # key_list = [4, 6, 7, 8, 1, 5]
     key_list = None
     df_loss_list, df_defu_list, df_opt_loss_list, df_opt_defu_list, df_init_loss_list, df_init_defu_list = read_file(file1, 'decision-focused', key_list)
     ts_loss_list, ts_defu_list, ts_opt_loss_list, ts_opt_defu_list, ts_init_loss_list, ts_init_defu_list = read_file(file2, 'two-stage', key_list)
+    hb_loss_list, hb_defu_list, hb_opt_loss_list, hb_opt_defu_list, hb_init_loss_list, hb_init_defu_list = read_file(file3, 'two-stage', key_list)
 
     print('Opt mean:', np.mean(df_opt_loss_list), np.mean(df_opt_defu_list))
     df_loss_median = np.median(df_loss_list - df_opt_loss_list)
@@ -142,6 +144,8 @@ if __name__=='__main__':
     init_defu_median = np.median(df_init_defu_list - df_opt_defu_list)
     ts_loss_median = np.median(ts_loss_list - ts_opt_loss_list)
     ts_defu_median = np.median(ts_defu_list - ts_opt_defu_list)
+    hb_loss_median = np.median(hb_loss_list - hb_opt_loss_list)
+    hb_defu_median = np.median(hb_defu_list - hb_opt_defu_list)
 
     df_loss_mean   = np.mean(df_loss_list - df_opt_loss_list)
     df_defu_mean   = np.mean(df_defu_list - df_opt_defu_list)
@@ -149,11 +153,13 @@ if __name__=='__main__':
     init_defu_mean = np.mean(df_init_defu_list - df_opt_defu_list)
     ts_loss_mean   = np.mean(ts_loss_list - ts_opt_loss_list)
     ts_defu_mean   = np.mean(ts_defu_list - ts_opt_defu_list)
+    hb_loss_mean   = np.mean(hb_loss_list - hb_opt_loss_list)
+    hb_defu_mean   = np.mean(hb_defu_list - hb_opt_defu_list)
     
-    bar_list = [(df_defu_mean,   ts_defu_mean,   init_defu_mean),
-                (df_defu_median, ts_defu_median, init_defu_median)]
-    print('mean (df, ts, init):', bar_list[0])
-    print('median (df, ts, init):', bar_list[1])
+    bar_list = [(df_defu_mean,   ts_defu_mean,   hb_defu_mean,   init_defu_mean),
+                (df_defu_median, ts_defu_median, hb_defu_median, init_defu_median)]
+    print('mean (df, ts, hb, init):', bar_list[0])
+    print('median (df, ts, hb, init):', bar_list[1])
     
     save_filename = "barchart_{}_n{}_p{}_b{}_noise{}.png".format(filename, GRAPH_N_LOW, GRAPH_E_PROB_LOW, DEFENDER_BUDGET, NOISE_LEVEL)
     generatePlot(bar_list, labels, save_filename)
--- generate_fig4.py
+++ generate_fig4.py
@@ -12,7 +12,7 @@ import argparse
 
 def return_yaxis(filename, method):
     f = open(filename, 'r')
-    max_epochs = 100
+    max_epochs = 200
     to_plot={'tr_loss': [[] for _ in range(max_epochs+1)],
              'val_loss':[[] for _ in range(max_epochs+1)],
              'te_loss': [[] for _ in range(max_epochs+1)],
@@ -78,28 +78,24 @@ def return_yaxis(filename, method):
     #return (to_plot,x,max_epochs+1)
     return (tr_loss, te_loss, tr_defu, te_defu, final_loss, final_defu, x)
 
-def generatePlot(xy_list, bar_list, filename):
+def generatePlot(xy_list, filename):
     
-    fig, axs = plt.subplots(1, len(xy_list) + len(bar_list))
+    fig, axs = plt.subplots(1, len(xy_list))
 
     #ax1 = fig.add_subplot(221)
     #ax2 = fig.add_subplot(222)
     #ax3 = fig.add_subplot(223)
     #ax4 = fig.add_subplot(224)
     for i in range(len(xy_list)):
-        x, y1, label1, y2, label2, title, ytitle = xy_list[i]
+        x, y1, label1, y2, label2, y3, label3, title, ytitle = xy_list[i]
         axs[i].plot(x, y1,'bo-' ,label=label1, markersize=1)
         axs[i].plot(x, y2, 'gs-' ,label=label2, markersize=1)
+        axs[i].plot(x, y3, 'r-' ,label=label3, markersize=1)
         
         axs[i].legend()
         # axs[i].title(title)
         # axs[i].xlabel('Epochs')
         # axs[i].ylabel(ytitle)
-
-    for i in range(len(bar_list)):
-        data_df, label_df, data_2s, label_2s = bar_list[i]
-        axs[len(xy_list) + i].bar(np.arange(2), [data_df, data_2s])
-        # axs[len(xy_list) + i].yticks(np.arange(2), (label_df, label_2d))
 
     #epochs = len(train_loss) - 1
     #x=range(-1, epochs)
@@ -109,7 +105,7 @@ def generatePlot(xy_list, bar_list, filename):
 
 if __name__=='__main__':
     
-    l=['DF-block','2S']
+    l=['DF-block','2S', 'Hybrid']
     # ==================== Parser setting ==========================
     parser = argparse.ArgumentParser(description='GCN Interdiction')
     parser.add_argument('--filename', type=str, help='filename under folder results')
@@ -132,10 +128,12 @@ if __name__=='__main__':
     #f="./results/0808normto10_linearphi_dist_tenruns_fullINIT_decision-focused_n20_p0.3_b2.0_global - Copy.csv"
     file1 = "results/random/{}_{}_n{}_p{}_b{}_noise{}.csv".format(filename, 'block-decision-focused', GRAPH_N_LOW, GRAPH_E_PROB_LOW, DEFENDER_BUDGET, NOISE_LEVEL)
     file2 = "results/random/{}_{}_n{}_p{}_b{}_noise{}.csv".format(filename, 'two-stage', GRAPH_N_LOW, GRAPH_E_PROB_LOW, DEFENDER_BUDGET, NOISE_LEVEL)
+    file3 = "results/random/{}_{}_n{}_p{}_b{}_noise{}.csv".format(filename, 'hybrid', GRAPH_N_LOW, GRAPH_E_PROB_LOW, DEFENDER_BUDGET, NOISE_LEVEL)
 
     #to_plot,x,epochs,=return_yaxis(f)
     tr_loss1, te_loss1, tr_defu1, te_defu1, final_loss1, final_defu1, x1 = return_yaxis(file1, 'decision-focused')
     tr_loss2, te_loss2, tr_defu2, te_defu2, final_loss2, final_defu2, x2 = return_yaxis(file2, 'two-stage')
+    tr_loss3, te_loss3, tr_defu3, te_defu3, final_loss3, final_defu3, x3 = return_yaxis(file3, 'hybrid')
     
     # generatePlot(x1,tr_loss1,l[0],tr_loss2,l[1],tr_loss3,l[2],tr_loss4,l[3], title="Training Loss", ytitle='KL Divergence')
     # generatePlot(x1,te_loss1,l[0],te_loss2,l[1],te_loss3,l[2],te_loss4,l[3], title="Testing Loss", ytitle='KL Divergence')
@@ -143,17 +141,13 @@ if __name__=='__main__':
     # generatePlot(x1,te_defu1,l[0],te_defu2,l[1],te_defu3,l[2],te_defu4,l[3],title="Testing Defender Utility", ytitle='Defender utility')
     
     xy_list = []
-    xy_list.append((x1, tr_loss1, l[0], tr_loss2, l[1], "Training Loss", 'KL Divergence'))
-    xy_list.append((x1, te_loss1, l[0], te_loss2, l[1], "Testing Loss",  'KL Divergence'))
-    xy_list.append((x1, tr_defu1, l[0], tr_defu2, l[1], "Training Defender Utility", 'Defender utility'))
-    xy_list.append((x1, te_defu1, l[0], te_defu2, l[1], "Testing Defender Utility",  'Defender utility'))
+    xy_list.append((x1, tr_loss1, l[0], tr_loss2, l[1], tr_loss3, l[2], "Training Loss", 'KL Divergence'))
+    xy_list.append((x1, te_loss1, l[0], te_loss2, l[1], te_loss3, l[2], "Testing Loss",  'KL Divergence'))
+    xy_list.append((x1, tr_defu1, l[0], tr_defu2, l[1], tr_defu3, l[2], "Training Defender Utility", 'Defender utility'))
+    xy_list.append((x1, te_defu1, l[0], te_defu2, l[1], te_defu3, l[2], "Testing Defender Utility",  'Defender utility'))
 
-    bar_list = [(final_loss1, l[0], final_loss2, l[1]), (final_defu1, l[0], final_defu2, l[1])]
-    print('loss:', bar_list[0])
-    print('defu:', bar_list[1])
-    
     save_filename = "{}_n{}_p{}_b{}_noise{}.png".format(filename, GRAPH_N_LOW, GRAPH_E_PROB_LOW, DEFENDER_BUDGET, NOISE_LEVEL)
-    generatePlot(xy_list, bar_list, save_filename)
+    generatePlot(xy_list, save_filename)
     
 
     
