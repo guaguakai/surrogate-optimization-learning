6155509dc22842a7fea3701218e8758a4a582c96
Kai Wang <guaguakai@boslogin02.rc.fas.harvard.edu>
2020-03-19 15:54:51 -0400

    clearning results

--- scripts/exp3.sh
+++
@@ -1,9 +0,0 @@
-FILENAME='1110-stochastic-server'
-
-NODES=30
-BUDGET=2
-PROB=0.2
-CUTSIZE='0.5n'
-SELECTION='coverage'
-
-python3 exp3.py --filename=$FILENAME --number-nodes=$NODES --budget=$BUDGET --prob=$PROB --cut-size=$CUTSIZE --block-selection=$SELECTION
