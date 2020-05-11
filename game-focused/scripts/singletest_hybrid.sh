b266edb29bf76dce663fad9a757de87526a14420
Kai Wang <guaguakai@boslogin01.rc.fas.harvard.edu>
2020-05-11 02:31:09 -0400

    adding 0510 results

---
+++ game-focused/scripts/singletest_hybrid.sh
@@ -0,0 +1,13 @@
+# FILENAME="0905-server-var0.1"
+# BUDGET=2
+# NODES=30
+# EPOCHS=101
+# PROB=0.2
+LR=0.001
+
+echo $NODES
+echo $VAR
+SEED=$VAR
+METHOD=4
+python3 blockQP.py --epochs=$EPOCHS --fixed-graph=0 --method=$METHOD --seed=$SEED --filename=$FILENAME --budget=$BUDGET --distribution=1 --number-nodes=$NODES --number-graphs=1 --number-samples=$SAMPLES --learning-rate=$LR --prob=$PROB --feature-size=16 --number-sources=5 --number-targets=5 --noise=$NOISE --cut-size=$CUTSIZE --block-selection=$SELECTION
+
